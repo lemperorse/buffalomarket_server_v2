535f9b6c4d66cbdc3bcd04125a36703a03695ee7
Pongvarid <pongvarid@gmail.com>
2021-06-11 11:26:19 +0700

    fix path

--- buffalo_market/wsgi.py
+++ buffalo_market/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buffalo_market.settings')
+#
+# application = get_wsgi_application()
 
-application = get_wsgi_application()
-
-
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
